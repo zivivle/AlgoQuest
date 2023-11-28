680034677f2e7fabdb4de67dfdb5c2d772473feb
zivivle <123868471+zivivle@users.noreply.github.com>
2023-11-28 10:31:37 +0900

    [Silver III] Title: 모든 순열, Time: 292 ms, Memory: 42372 KB -BaekjoonHub

--- 백준/Silver/10974. 모든 순열/모든 순열.js
+++ 백준/Silver/10974. 모든 순열/모든 순열.js
@@ -1,29 +1,28 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split('\n')
-
 let n = Number(input[0])
+
 let arr = []
-for(let i=1; i<=n; i++) arr.push(i);
-let visited = new Array(n).fill(false);
+for(let i=1; i<=n; i++) arr.push(i)
+
+let visited = new Array(n).fill(false)
 let selected = [];
 
 let answer = ""
-function dfs(arr, depth) {
+function fac(arr, depth){
     if(depth == n){
-        let result = [];
-        for(let i of selected) result.push(arr[i])
-        for(let x of result) answer += x + " ";
-        answer += "\n";
-        return // 중요
+        for(let x of selected) answer += x + " ";
+        answer += "\n"
+        return;
     }
     for(let i=0; i<arr.length; i++){
         if(visited[i]) continue;
-        selected.push(i);
+        selected.push(arr[i])
         visited[i] = true;
-        dfs(arr, depth + 1);
+        fac(arr, depth + 1)
         selected.pop();
         visited[i] = false;
     }
 }
-dfs(arr, 0);
+fac(arr, 0)
 console.log(answer)
