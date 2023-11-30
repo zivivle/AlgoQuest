e107c121310b793a069f38f132cc4e6ded7bf6d1
zivivle <123868471+zivivle@users.noreply.github.com>
2023-11-30 20:54:43 +0900

    [level unrated] Title: 이진수 더하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120885. 이진수 더하기/이진수 더하기.js
@@ -0,0 +1,10 @@
+function solution(bin1, bin2) {
+    var answer = '';
+    // 10진수로 변환
+    let num1 = parseInt(bin1, 2)
+    let num2 = parseInt(bin2, 2)
+    
+    answer = num1 + num2
+    
+    return answer.toString(2);
+}
