6babdb9d8bbbccf922cbf1c554d6ff3f392f0416
zivivle <123868471+zivivle@users.noreply.github.com>
2024-04-10 16:22:59 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    var answer = '';
+    
+    for(let i=1; i<n+1; i++){
+        if(i%2 == 1) answer += '수'
+        else answer += '박'
+    }
+    
+    return answer;
+}
