f1c9d91be1791db22077e0645c9061c8e7ac1766
zivivle <123868471+zivivle@users.noreply.github.com>
2024-04-10 16:12:59 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.05 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -0,0 +1,13 @@
+function solution(phone_number) {
+    var answer = '';
+    
+    for(let i=0; i<phone_number.length; i++){
+        if(i >= phone_number.length - 4){
+            answer += phone_number[i];
+        } else {
+            answer += '*'
+        }
+    }
+    
+    return answer;
+}
