36f37c7ac58fbeda8da471ffdaac62e4bf9a72d1
zivivle <123868471+zivivle@users.noreply.github.com>
2024-11-14 07:41:22 +0900

    [level 0] Title: 안전지대, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.js
+++ 프로그래머스/0/120866. 안전지대/안전지대.js
@@ -1,26 +1,13 @@
 function solution(board) {
-    let temArr = board
-    const n = board.length
-    const dx = [0, 1, 1, 1, 0, -1, -1, -1]
-    const dy = [-1, -1, 0, 1, 1, 1, 0, -1]
+    const arr = board.flatMap((row, i) => 
+  row.map((value, j) => ({ value, i, j }))
+)
+    console.log('board', arr)
     
-    for(let i=0; i<n; i++){
-        for(let j=0; j<n; j++){
-            for(let d=0; d<dx.length; d++){
-                let x = j + dx[d]
-                let y = i + dy[d]
-                
-                if (y >= 0 && y < n && x >= 0 && x < n) {
-                    if(temArr[i][j] == 1){
-                        if(temArr[y][x] == 1) continue
-                        temArr[y][x] = 2
-                    }
-                }
-            }
-        }
-    }
+    const parsedArr = arr.filter(obj => obj.value != 1)
     
-    const result = temArr.flat().filter(x => x == 0)
+    console.log('parsed', parsedArr)
     
-    return result.length;
+   
+    return 0;
 }
